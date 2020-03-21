98a7f83affebaaf1c4947c061f5a0a72022caf28
jonatas1n <jonatasgeek@gmail.com>
2020-03-21 01:42:17 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let textbox = document.getElementsByClassName('textbox');
 let buttons = document.getElementsByClassName('btn');
 let capButton = document.getElementsByClassName('add-cap');
+const artigo = new RegExp("Art. [0-9]{1,}o");
+
 capButton = capButton[0];
 textbox = textbox[0];
 
 for (let btn of buttons){
-    btn.addEventListener('click', () =>{
+    btn.addEventListener('click', () => {
         let cmd = btn.dataset['command'];
         document.execCommand(cmd, false, null);
     })
@@ -18,6 +20,4 @@ capButton.addEventListener('click', () => {
     var novo = velho.cloneNode(true);
     novo.getElementsByClassName("titulo")[0].innerHTML = "Capitulo " + String(parseInt(document.getElementsByClassName("capitulo").length) + 1);
     textbox.appendChild(novo);
-    
 });
-
