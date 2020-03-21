038065915d41ef3b9f99734ff70651e6cd853b3d
jonatas1n <jonatasgeek@gmail.com>
2020-03-21 12:59:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-let textbox = document.getElementsByClassName('textbox');
-let buttons = document.getElementsByClassName('btn');
+const buttons = document.getElementsByClassName('btn');
 let capButton = document.getElementsByClassName('add-cap');
-const artigo = new RegExp("Art. [0-9]{1,}o");
+// const artigo = new RegExp("Art. [0-9]{1,}o");
 
 capButton = capButton[0];
-textbox = textbox[0];
 
 for (let btn of buttons){
     btn.addEventListener('click', () => {
@@ -13,11 +11,16 @@ for (let btn of buttons){
     })
 };
 
-capButton.addEventListener('click', () => {
-    var velho = document.getElementsByClassName("capitulo");
-    velho = velho[0];
-    console.log(velho);
-    var novo = velho.cloneNode(true);
-    novo.getElementsByClassName("titulo")[0].innerHTML = "Capitulo " + String(parseInt(document.getElementsByClassName("capitulo").length) + 1);
-    textbox.appendChild(novo);
-});
+for (let i = 0; i < document.getElementsByClassName("abrev-artigo").length; i++){
+    document.getElementsByClassName('abrev-artigo')[i].innerHTML = "Art. " + parseInt(i + 1) + '.';
+}
+
+// capButton.addEventListener('click', () => {
+//     var velho = document.getElementsByClassName("capitulo");
+//     velho = velho[0];
+//     var novo = velho.cloneNode(true);
+//     novo.getElementsByClassName("titulo")[0].innerHTML = "Capitulo " + String(parseInt(document.getElementsByClassName("capitulo").length) + 1);
+//     let normativa = document.getElementsByClassName("normativa")[0];
+//     normativa.appendChild(novo);
+// });
+
