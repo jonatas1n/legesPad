132438fbf1c4945bbf6973edd3b71d4e4bbca141
jonatas1n <jonatasgeek@gmail.com>
2020-03-23 16:45:04 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let parser = new DOMParser();
 normativa = document.getElementsByClassName("normativa")[0];
-// let capitulo = parser.parseFromString("capitulo.html", "text/html");
+let capitulo = parser.parseFromString("capitulo.html", "text/html");
 
 // Checa a inexistência de capitulos. Caso positivo, cria um novo.
 
 let divCapitulo = document.createElement('div');
 divCapitulo.classList.add('capitulo');
+
 let titulo = document.createElement('div');
 titulo.classList.add('titulo');
 let subtitulo = document.createElement('div');
@@ -18,17 +19,19 @@ if (document.getElementsByClassName('capitulo').length == 0){
     normativa.appendChild(divCapitulo);
 }
 
+// Adiciona artigos automaticamente
 let artigo = document.getElementsByClassName('artigo');
 if(artigo.length == 0){
     let divAbrev = document.createElement('div');
     divAbrev.contentEditable = 'false';
     divAbrev.classList.add('abrev-artigo');
+
     let divbase = document.createElement('div');
-    divbase.innerHTML = "Escreva seu artigo";
+    divbase.innerHTML = "Escreva o artigo";
     divbase.classList.add('artigo');
     divbase.appendChild(divAbrev);
     divbase.prepend(divAbrev);
-    
+
     normativa.appendChild(divbase);
 }
 
@@ -48,11 +51,3 @@ for (let i = 0; i < abrevArtigo.length; i++){
         abrevArtigo[i].innerHTML = "Art. " + parseInt(i + 1) + '. ';
     }
 }
-
-
-// Analisar função
-
-// let normativa = document.getElementsByClassName('normativa')[0];
-// function adicionaCapitulo(){
-//     normativa.appendChild();
-// }
